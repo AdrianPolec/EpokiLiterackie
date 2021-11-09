4eb8b7810c0bd9ce8fb9622ee72cd1a8e4e0b001
Adrian Polec <adrian.polec98@gmail.com>
2021-11-09 08:52:06 +0100

    final commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,28 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './styles/app.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navi from "./js/Components/Navi/Navi";
+import Epoki from "./js/Components/Epoki literackie/Epoki";
 
-// any CSS you import will output into a single css file (app.css in this case)
-import '../styles/app.css';
+class App extends Component {
+    render() {
+        return (
+            <Router>
+                <Navi/>
+                <Switch>
+                    <Route path="/EpokiLiterackie">
+                        <Epoki/>
+                    </Route>
+                    <Route path="/Home">
+                    </Route>
+                    <Route path="/DziełaIpostacie">
 
-// start the Stimulus application
-import './bootstrap';
+                    </Route>
+                </Switch>
+            </Router>
+        );
+    }
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
